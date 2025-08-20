3fec44c1e666a867da513de56dcf56d91e17d010
Jão <joao7778882@gmail.com>
2025-08-19 21:59:23 -0300

    update features in PetResource and fixed bugs in PetSize.java.

--- src/main/java/com/vetclinic/vetclinic/Enum/PetSize.java
+++ src/main/java/com/vetclinic/vetclinic/Enum/PetSize.java
@@ -1,12 +1,12 @@
 package com.vetclinic.vetclinic.Enum;
 
 public enum PetSize {
-    VERY_SMALL("Very Small"),
+    VERYSMALL("Very Small"),
     SMALL("Small"),
     MEDIUM("Medium"),
     LARGE("Large"),
-    X_LARGE("X Large"),
-    XX_LARGE("XX Large");
+    XLARGE("X Large"),
+    XXLARGE("XX Large");
 
 
     private final String description;
